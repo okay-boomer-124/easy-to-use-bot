27158896f44cc8c0e78a64fdf015742328ad8bfa
okay-boomer-124 <72575138+okay-boomer-124@users.noreply.github.com>
2021-10-31 01:26:19 +0000

    Create id.js

---
+++ commands/id.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "name",
+  desc: "Replies with your discord name",
+  async execute(client, message, args) {
+
+  message.channel.send(`You are ${message.author.id}`)
+
+
+
+}
+}
